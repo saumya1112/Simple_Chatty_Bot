b0ccb12ee8d2d3ed5e6655dd1810dc62898ae4ee
saumya gupta <saumya.gupta_cs18@gla.ac.in>
2020-04-10 11:58:04 +0530

    add "!" in yourname

---
+++ Simple Chatty Bot/task/src/bot/SimpleBot.java
@@ -0,0 +1,24 @@
+package bot;
+
+import java.util.Scanner;
+
+public class SimpleBot {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Hello! My name is Aid.");
+        System.out.println("I was created in 2018.");
+        System.out.println("Please, remind me your name.");
+
+        // reading a name
+        String yourName = scanner.nextLine();
+        System.out.println("What a great name you have," + " ");
+        System.out.println(yourName + "!");
+        System.out.println("Let me guess your age.");
+        System.out.println("Enter remainders of dividing your age by 3, 5 and 7.");
+
+        // reading all remainders
+
+        System.out.println("Your age is {yourAge}; that's a good time to start programming!");
+    }
+}
