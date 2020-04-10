a89570661917487b0274c2d683095e2fa7c6bc45
saumya gupta <saumya.gupta_cs18@gla.ac.in>
2020-04-10 13:22:56 +0530

    add "!" in yourname

--- Simple Chatty Bot/task/src/bot/SimpleBot.java
+++ Simple Chatty Bot/task/src/bot/SimpleBot.java
@@ -10,22 +10,29 @@ public class SimpleBot {
         System.out.println("I was created in 2018.");
         System.out.println("Please, remind me your name.");
 
-        // reading a name
-        String yourName = scanner.nextLine();
-        System.out.println("What a great name you have," + " ");
-        System.out.println(yourName + "!");
+        String name = scanner.nextLine();
+
+        System.out.println("What a great name you have, " + name + "!");
         System.out.println("Let me guess your age.");
         System.out.println("Enter remainders of dividing your age by 3, 5 and 7.");
 
-        // reading all remainders
         int rem3 = scanner.nextInt();
         int rem5 = scanner.nextInt();
         int rem7 = scanner.nextInt();
-        int yourAge = (rem3 * 70 + rem5 * 21 + rem7 * 15) % 105;
 
-        System.out.print("Your age is" + " ");
-        System.out.print(yourAge + ";");
-        System.out.println("that's a good time to start programming!");
+        int age = (rem3 * 70 + rem5 * 21 + rem7 * 15) % 105;
 
+        System.out.println("Your age is " + age + "; that's a good time to start programming!");
+        System.out.println("Now I will prove to you that I can count to any number you want.");
+
+        // read a number and count to it here
+
+        int num = scanner.nextInt();
+        for (int i = 0; i <= num; i++) {
+            System.out.println(i + "!");
+        }
+        System.out.println("Completed, have a nice day!");
     }
 }
+
+
